6c27943d0fd700c3967bb0dcf59e6a0cb388db2e
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-28 20:30:43 +0100

    Add credit card to customers.

--- src/main/java/StripeApp.java
+++ src/main/java/StripeApp.java
@@ -1,6 +1,8 @@
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
+import com.stripe.model.Token;
+import com.stripe.param.CustomerRetrieveParams;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,23 @@ public class StripeApp {
 
 //        Customer newCustomer = Customer.create(customerParameter);
 
-        Customer a = Customer.retrieve("cus_K7m83jddYXLErA");
+        CustomerRetrieveParams params = CustomerRetrieveParams.builder().addExpand("sources").build();
+        Customer a = Customer.retrieve("cus_K7m83jddYXLErA", params, null);
 
+        Map<String, Object> cardParameter = new HashMap<String, Object>();
+        cardParameter.put("number", "4242424242424242");
+        cardParameter.put("exp_month", "11");
+        cardParameter.put("exp_year", "2022");
+        cardParameter.put("cvc", "123");
+
+        Map<String, Object> tokenParam = new HashMap<String, Object>();
+        tokenParam.put("card", cardParameter);
+
+        Token token = Token.create(tokenParam);
+
+        Map<String, Object> source = new HashMap<String, Object>();
+        source.put("source", token.getId());
+
+        a.getSources().create(source);
     }
 }
