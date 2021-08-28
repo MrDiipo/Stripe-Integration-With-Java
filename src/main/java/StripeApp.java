5767964be0cd34c0f39c9ae910cdd0857cd883ee
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-28 20:44:20 +0100

    Charge customers test.

--- src/main/java/StripeApp.java
+++ src/main/java/StripeApp.java
@@ -1,5 +1,6 @@
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 import com.stripe.model.Customer;
 import com.stripe.model.Token;
 import com.stripe.param.CustomerRetrieveParams;
@@ -17,23 +18,31 @@ public class StripeApp {
 
 //        Customer newCustomer = Customer.create(customerParameter);
 
-        CustomerRetrieveParams params = CustomerRetrieveParams.builder().addExpand("sources").build();
-        Customer a = Customer.retrieve("cus_K7m83jddYXLErA", params, null);
+//        CustomerRetrieveParams params = CustomerRetrieveParams.builder().addExpand("sources").build();
+        Customer a = Customer.retrieve("cus_K7m83jddYXLErA");
 
-        Map<String, Object> cardParameter = new HashMap<String, Object>();
-        cardParameter.put("number", "4242424242424242");
-        cardParameter.put("exp_month", "11");
-        cardParameter.put("exp_year", "2022");
-        cardParameter.put("cvc", "123");
+//        Map<String, Object> cardParameter = new HashMap<String, Object>();
+//        cardParameter.put("number", "4242424242424242");
+//        cardParameter.put("exp_month", "11");
+//        cardParameter.put("exp_year", "2022");
+//        cardParameter.put("cvc", "123");
+//
+//        Map<String, Object> tokenParam = new HashMap<String, Object>();
+//        tokenParam.put("card", cardParameter);
+//
+//        Token token = Token.create(tokenParam);
+//
+//        Map<String, Object> source = new HashMap<String, Object>();
+//        source.put("source", token.getId());
+//
+//        a.getSources().create(source);
 
-        Map<String, Object> tokenParam = new HashMap<String, Object>();
-        tokenParam.put("card", cardParameter);
+         Map<String, Object> chargeParam = new HashMap<String, Object>();
+         chargeParam.put("amount", "500");
+         chargeParam.put("currency", "usd");
+         chargeParam.put("customer", a.getId());
 
-        Token token = Token.create(tokenParam);
-
-        Map<String, Object> source = new HashMap<String, Object>();
-        source.put("source", token.getId());
-
-        a.getSources().create(source);
+        Charge.create(chargeParam);
+        System.out.println(a);
     }
 }
